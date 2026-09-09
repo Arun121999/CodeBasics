43b596f8c8cf377cfdd0414b7a9b47c18d5af3f9
Arun <as894764@gmail.com>
2026-09-09 17:27:15 +0530

    Python Sets Basics

---
+++ python/python-sets.py
@@ -0,0 +1,21 @@
+#Creating a set — no duplicates allowed
+# things = {"fruits","vegetables","toys","fruits"}
+# print(things)
+
+# team_a = {"Arun","Abhishek","Sahil"}
+# team_b = {"Sahil","Romio","Jack"}
+
+
+#.union() — combine two sets & remove the dublicate values
+# all_members_union = team_a.union(team_b)
+# print(all_members_union)
+
+
+#.intersection() — give the common values & remove the other values
+# all_members_intersection = team_a.intersection(team_b)
+# print(all_members_intersection)
+
+
+# difference() — give the values which are not in the second which are present in first
+# all_members_difference = team_a.difference(team_b)
+# print(all_members_difference)
